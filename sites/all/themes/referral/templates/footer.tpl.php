615ce534e57eda019b65d4f9901169c1f47b7550
rvmohan <mohan.rv@igate.com>
2015-08-28 19:38:44 +0530

    Initial Drupal commit

---
+++ sites/all/themes/referral/templates/footer.tpl.php
@@ -0,0 +1,30 @@
+<div id="footer"><div class="section">
+      <?php //print render($page['footer']); ?>
+      	<div class="call">
+	      	<div class="call-icon"><img src="/<?php  print drupal_get_path('theme', 'referral'); ?>/images/phone.png"></div>
+	      	<div class="call-text">
+	      		<p><span class="title">Call</span></p>
+	      		<p><span>T: 123-456-7890</span></p>
+	      	</div>
+      	</div>
+      	<div class="contact">
+	      	<div class="contact-icon"><img src="/<?php  print drupal_get_path('theme', 'referral'); ?>/images/letter.png"></div>
+	      	<div class="contact-text">
+	      		<p><span class="title">Contact</span></p>
+	      		<p><span>info@mysite.com</span></p>
+	      	</div>
+      	</div>
+      	<div class="followme">
+	      	<div class="followme-icon"><img src="/<?php  print drupal_get_path('theme', 'referral'); ?>/images/followme.png"></div>
+	      	<div class="followme-text">
+	      		<p><span class="title">Follow me</span></p>
+	      		<p><span>info@mysite.com</span></p>
+	      	</div>
+      	</div>
+      	<div class="copyright">
+	      	<div class="copyright-icon"><img src="/<?php  print drupal_get_path('theme', 'referral'); ?>/images/copyright.png"></div>
+	      	<div class="copyright-text">
+	      		<p><span>&copy;  2023 by The Mobilizers.</span></p>
+	      	</div>
+      	</div>
+    </div></div> <!-- /.section, /#footer -->
